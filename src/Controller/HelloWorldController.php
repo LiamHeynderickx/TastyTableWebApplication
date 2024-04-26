20abc5353840bb40c23af5b9e08efb4955437bc9
mithil <mithill2002@gmail.com>
2024-04-26 14:56:02 +0200

    test

---
+++ src/Controller/HelloWorldController.php
@@ -0,0 +1,33 @@
+<?php
+// src/Controller/HelloWorldController.php
+// src/Controller/HelloWorldController.php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+//use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HelloWorldController
+{
+
+    #[Route('/welcome/{name}')]
+
+    public function welcome(string $name): Response
+    {
+        $htmlContent = "<!DOCTYPE html>
+<html lang=\"en\">
+<head>
+    <meta charset=\"UTF-8\">
+    <title>Welcome big dawg</title>
+</head>
+<body>
+    <h1>Welcome, " . htmlspecialchars($name, ENT_QUOTES, 'UTF-8') . "!</h1>
+</body>
+</html>";
+
+        return new Response($htmlContent);
+    }
+}
+
