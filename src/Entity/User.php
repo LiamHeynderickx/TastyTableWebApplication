3b84839fb349a6e1f9156b1d2d4b05e1be301aa8
mithil <mithill2002@gmail.com>
2024-05-18 22:12:50 +0200

    added tables following, followers, comments, posts, all with entity relations

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\GeneratedValue;
@@ -35,6 +37,38 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column(name:"dietPreference")]
     private string $dietPreference;
+
+    /**
+     * @var Collection<int, Following>
+     */
+    #[ORM\OneToMany(targetEntity: Following::class, mappedBy: 'followingId', orphanRemoval: true)]
+    private Collection $userFollowing;
+
+    /**
+     * @var Collection<int, Followers>
+     */
+    #[ORM\OneToMany(targetEntity: Followers::class, mappedBy: 'followerId', orphanRemoval: true)]
+    private Collection $userFollowers;
+
+    /**
+     * @var Collection<int, Posts>
+     */
+    #[ORM\OneToMany(targetEntity: Posts::class, mappedBy: 'creatorId', orphanRemoval: true)]
+    private Collection $userPosts;
+
+    /**
+     * @var Collection<int, Comments>
+     */
+    #[ORM\OneToMany(targetEntity: Comments::class, mappedBy: 'userId', orphanRemoval: true)]
+    private Collection $userComments;
+
+    public function __construct()
+    {
+        $this->userFollowing = new ArrayCollection();
+        $this->userFollowers = new ArrayCollection();
+        $this->userPosts = new ArrayCollection();
+        $this->userComments = new ArrayCollection();
+    }
 
     public function getId(): int
     {
@@ -126,5 +160,125 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function getUserIdentifier(): string
     {
         return $this->username;
+    }
+
+    /**
+     * @return Collection<int, Following>
+     */
+    public function getUserFollowing(): Collection
+    {
+        return $this->userFollowing;
+    }
+
+    public function addUserFollowing(Following $userFollowing): static
+    {
+        if (!$this->userFollowing->contains($userFollowing)) {
+            $this->userFollowing->add($userFollowing);
+            $userFollowing->setUserId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUserFollowing(Following $userFollowing): static
+    {
+        if ($this->userFollowing->removeElement($userFollowing)) {
+            // set the owning side to null (unless already changed)
+            if ($userFollowing->getUserId() === $this) {
+                $userFollowing->setUserId(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Followers>
+     */
+    public function getUserFollowers(): Collection
+    {
+        return $this->userFollowers;
+    }
+
+    public function addUserFollower(Followers $userFollower): static
+    {
+        if (!$this->userFollowers->contains($userFollower)) {
+            $this->userFollowers->add($userFollower);
+            $userFollower->setFollowerId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUserFollower(Followers $userFollower): static
+    {
+        if ($this->userFollowers->removeElement($userFollower)) {
+            // set the owning side to null (unless already changed)
+            if ($userFollower->getFollowerId() === $this) {
+                $userFollower->setFollowerId(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Posts>
+     */
+    public function getUserPosts(): Collection
+    {
+        return $this->userPosts;
+    }
+
+    public function addUserPost(Posts $userPost): static
+    {
+        if (!$this->userPosts->contains($userPost)) {
+            $this->userPosts->add($userPost);
+            $userPost->setCreatorId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUserPost(Posts $userPost): static
+    {
+        if ($this->userPosts->removeElement($userPost)) {
+            // set the owning side to null (unless already changed)
+            if ($userPost->getCreatorId() === $this) {
+                $userPost->setCreatorId(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Comments>
+     */
+    public function getUserComments(): Collection
+    {
+        return $this->userComments;
+    }
+
+    public function addUserComment(Comments $userComment): static
+    {
+        if (!$this->userComments->contains($userComment)) {
+            $this->userComments->add($userComment);
+            $userComment->setUserId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUserComment(Comments $userComment): static
+    {
+        if ($this->userComments->removeElement($userComment)) {
+            // set the owning side to null (unless already changed)
+            if ($userComment->getUserId() === $this) {
+                $userComment->setUserId(null);
+            }
+        }
+
+        return $this;
     }
 }
